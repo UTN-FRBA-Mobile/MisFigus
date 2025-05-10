c0b709f0a8701d4d18aab8df5b167088fcd4c214
Belen Seoane <seoane.m.b@gmail.com>
2025-05-10 19:41:07 -0300

    posible trader

--- app/src/main/java/com/misfigus/screens/TradingScreen.kt
+++ app/src/main/java/com/misfigus/screens/TradingScreen.kt
@@ -1,30 +1,35 @@
 package com.misfigus.screens
 
+import com.example.misfigus.R
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Error
-import androidx.compose.material.icons.filled.Star
+import androidx.compose.material.icons.automirrored.filled.ArrowForward
+import androidx.compose.material.icons.outlined.Error
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-import com.misfigus.ui.theme.Background
 import com.misfigus.ui.theme.CardColor
 import com.misfigus.ui.theme.EditColor
+import com.misfigus.ui.theme.Grey
 import com.misfigus.ui.theme.White
 
 @Composable
-fun IntercambioScreen() {
+fun TraddingBanner() {
     Card(
         modifier = Modifier
             .padding(start = 16.dp, top = 20.dp, end = 16.dp)
@@ -48,7 +53,7 @@ fun IntercambioScreen() {
                 verticalAlignment = Alignment.CenterVertically
             ) {
                 Icon(
-                    imageVector = Icons.Filled.Error,
+                    imageVector = Icons.Outlined.Error,
                     contentDescription = "Error Icon",
                     tint = CardColor
                 )
@@ -61,4 +66,81 @@ fun IntercambioScreen() {
             }
         }
     }
+}
+
+@Composable
+fun TraderCard() {
+    Card(
+        modifier = Modifier
+            .padding(start = 16.dp, top = 20.dp, end = 16.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = White
+        )
+    ) {
+        Row(
+            modifier = Modifier.padding(16.dp),
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Image(
+                painter = painterResource(id = R.drawable.matias),
+                contentDescription = "Profile Picture",
+                modifier = Modifier
+                    .width(40.dp)
+                    .height(40.dp),
+                contentScale = ContentScale.Crop
+            )
+            Spacer(modifier = Modifier.width(10.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = "Matías, Caballito",
+                    fontSize = 13.sp,
+                    color = Color.Black,
+                    fontWeight = FontWeight.SemiBold,
+                    modifier = Modifier.padding(bottom = 3.dp)
+                )
+//               Row(
+//                verticalAlignment = Alignment.CenterVertically
+//                ) {
+//                    Icon(
+//                        imageVector = Icons.Outlined.Book,
+//                        contentDescription = "Book Icon",
+//                        tint = CardColor
+//                    )
+//                    Spacer(modifier = Modifier.width(4.dp))
+//                    Text(
+//                        text = "Fifa World Cup Qatar 2022",
+//                        fontSize = 13.sp,
+//                        color = CardColor
+//                    )
+//                }
+                Text(
+                    text = "Fifa World Cup Qatar 2022",
+                    fontSize = 13.sp,
+                    color = CardColor,
+                    modifier = Modifier.padding(bottom = 3.dp)
+                )
+                Text(
+                    text = "Tiene 6 figuritas que te faltan",
+                    fontSize = 13.sp,
+                    color = Grey
+                )
+            }
+            Icon(
+                imageVector = Icons.AutoMirrored.Filled.ArrowForward,
+                contentDescription = "Canjear",
+                tint = EditColor
+            )
+        }
+    }
+}
+
+@Composable
+fun IntercambioScreen() {
+    Column() {
+        TraddingBanner()
+        TraderCard()
+    }
+
 }
