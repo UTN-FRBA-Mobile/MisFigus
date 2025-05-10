e036cdfd48b17a3018e7fae93fb85021789b9321
Belen Seoane <seoane.m.b@gmail.com>
2025-05-10 18:27:15 -0300

    canje title

--- app/src/main/java/com/misfigus/screens/TradingScreen.kt
+++ app/src/main/java/com/misfigus/screens/TradingScreen.kt
@@ -1,12 +1,64 @@
 package com.misfigus.screens
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Error
+import androidx.compose.material.icons.filled.Star
 import androidx.compose.material3.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import com.misfigus.ui.theme.Background
+import com.misfigus.ui.theme.CardColor
+import com.misfigus.ui.theme.EditColor
+import com.misfigus.ui.theme.White
 
 @Composable
 fun IntercambioScreen() {
-    Text("Pantalla de Intercambio", modifier = Modifier.padding(32.dp))
+    Card(
+        modifier = Modifier
+            .padding(start = 16.dp, top = 20.dp, end = 16.dp)
+            .fillMaxWidth(),
+        shape = RoundedCornerShape(16.dp),
+        colors = CardDefaults.cardColors(
+            containerColor = White
+        )
+    ) {
+        Column {
+            Text(
+                "Canje",
+                color = Color.Black,
+                fontWeight = FontWeight.SemiBold,
+                fontSize = 35.sp,
+                modifier = Modifier.padding(start = 16.dp, top = 16.dp, bottom = 8.dp)
+            )
+
+            Row(
+                modifier = Modifier.padding(start = 16.dp, bottom = 20.dp),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Error,
+                    contentDescription = "Error Icon",
+                    tint = CardColor
+                )
+                Spacer(modifier = Modifier.width(4.dp))
+                Text(
+                    text = "Hay 15 personas de tu zona que quieren canjear",
+                    fontSize = 13.sp,
+                    color = CardColor
+                )
+            }
+        }
+    }
 }
