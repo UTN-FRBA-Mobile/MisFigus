f8d626ea8ff4863265c19785b44a73de6f1da5ec
Belen Seoane <seoane.m.b@gmail.com>
2025-05-11 00:13:28 -0300

    search bar y boton de crear

--- app/src/main/java/com/misfigus/screens/AlbumsScreen.kt
+++ app/src/main/java/com/misfigus/screens/AlbumsScreen.kt
@@ -1,12 +1,15 @@
 package com.misfigus.screens
 
-import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.grid.GridCells
 import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.lazy.grid.items
@@ -15,24 +18,103 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
-import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Search
+import androidx.compose.material.icons.outlined.Add
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.setValue
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.unit.sp
 
 import com.misfigus.components.AlbumItem
 import com.misfigus.navigation.BackButton
 import com.misfigus.navigation.mockedAlbums
+import com.misfigus.ui.theme.EditColor
+import com.misfigus.ui.theme.White
+
+@Composable
+fun SearchBar(
+    query: String,
+    onQueryChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    placeholder: String = "Buscar"
+) {
+    TextField(
+        value = query,
+        onValueChange = onQueryChange,
+        placeholder = { Text(placeholder) },
+        leadingIcon = {
+            Icon(
+                imageVector = Icons.Default.Search,
+                contentDescription = "Search Icon"
+            )
+        },
+        singleLine = true,
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(16.dp),
+        shape = RoundedCornerShape(12.dp)
+    )
+}
+
+@Composable
+fun NewTag() {
+    Row(
+        modifier = Modifier
+            .padding(end = 16.dp)
+            .clip(RoundedCornerShape(12.dp))
+            .background(EditColor)
+            .padding(horizontal = 12.dp, vertical = 3.dp),
+        verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Nuevo",
+            fontSize = 15.sp,
+            color = White
+        )
+        Spacer(modifier = Modifier.width(6.dp))
+        IconButton(onClick = {}) { // TODO add screen
+            Icon(
+                imageVector = Icons.Outlined.Add,
+                contentDescription = "Add",
+                tint = White
+            )
+        }
+    }
+}
 
 @Composable
 fun AlbumsScreen(navHostController: NavHostController, category: String) {
-
+    var searchQuery by remember { mutableStateOf("") }
     val albums = mockedAlbums().filter {  category.equals(it.category.toString()) }
+
     Scaffold(
         topBar = { BackButton(navHostController, "Categorias") }
     ) { innerPadding ->
         Column(modifier = Modifier.fillMaxSize().padding(innerPadding)) {
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                verticalAlignment = Alignment.CenterVertically
+            ) {
+                SearchBar( // TODO agregar funcionalidad
+                    query = searchQuery,
+                    onQueryChange = { searchQuery = it },
+                    modifier = Modifier.weight(1f)
+                )
+                NewTag()
+            }
+
             Text(
                 text = category.first().uppercaseChar() + category.drop(1).lowercase(),
                 modifier = Modifier.padding(20.dp),
@@ -42,7 +124,6 @@ fun AlbumsScreen(navHostController: NavHostController, category: String) {
                 columns = GridCells.Fixed(1),
                 modifier = Modifier.fillMaxWidth().fillMaxHeight().padding(16.dp)
             ) {
-
                 items(albums) { abm ->
                     AlbumItem(
                         album = abm,
