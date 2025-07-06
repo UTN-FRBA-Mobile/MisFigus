b21b2a3c36f922333534b96181e71f55b411e198
Agustin <agtineus@gmail.com>
2025-07-06 19:37:37 -0300

    Kiosco terminado. Cambio color barra superior

--- app/src/main/java/com/misfigus/screens/map/MapScreen.kt
+++ app/src/main/java/com/misfigus/screens/map/MapScreen.kt
@@ -1,6 +1,7 @@
 package com.misfigus.screens.map
 
 import android.Manifest
+import android.annotation.SuppressLint
 import android.content.pm.PackageManager
 import android.os.Bundle
 import androidx.activity.compose.rememberLauncherForActivityResult
@@ -66,6 +67,7 @@ fun distanceBetween(lat1: Double, lon1: Double, lat2: Double, lon2: Double): Dou
     return earthRadius * c
 }
 
+@SuppressLint("UnusedBoxWithConstraintsScope")
 @Composable
 fun MapScreen() {
     val context = LocalContext.current
@@ -173,8 +175,11 @@ fun MapScreen() {
             mapView.getMapAsync { googleMap: GoogleMap ->
                 googleMap.uiSettings.isMyLocationButtonEnabled = false
                 googleMap.setOnMarkerClickListener { marker ->
-                    selectedKiosk = kiosks.find { it.name == marker.title }
-                    marker.showInfoWindow()
+                    val kiosk = kiosks.find { it.name == marker.title }
+                    if (kiosk != null) {
+                        selectedKiosk = kiosk
+                        kioskDetailShown = kiosk // <-- esto abre la vista detallada directamente
+                    }
                     true
                 }
                 if (hasLocationPermission) {
